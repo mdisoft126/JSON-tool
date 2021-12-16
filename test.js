eb7fb54b20fcb91e49cb1c7f8dc7d39d44f470fa
Marcin Dolecki | Kramp <marcin.dolecki@kramp.com>
2021-12-16 12:33:52 +0100

    converted to desired schema and result in json

--- test.js
+++ test.js
@@ -1,29 +1,36 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', './person.json');
+ourRequest.open('GET', './ratings/response ' + '(5)' + '.json');
 ourRequest.onload = function() {
     var ourData = JSON.parse(ourRequest.responseText);
     console.log(ourData.ratings.worker);
-    //rated unrated
+//rated unrated
     var rated = ourData.ratings.worker.rated;
     console.log("rated: " + rated);
     var unrated = ourData.ratings.worker.unrated;
     console.log("unrated: " + unrated);
-    //all
-    var all = ourData.ratings.worker.all.avg;
-    console.log("all_ratings.avg: " + all);
-    var all = "all_ratings.std: " + ourData.ratings.worker.all.std;
+//all
+    var all_avg = ourData.ratings.worker.all.avg;
+    console.log("all_ratings.avg: " + all_avg);
+    var all_std = ourData.ratings.worker.all.std;
     // all = "all_ratings.std: " + all;
-    // console.log("all_ratings.std: " + all);
-    console.log(all);
-    //source
-    var s = ourData.ratings.worker.q[0].avg;
-    console.log("src_ratings.avg: " + s);
-    var s = ourData.ratings.worker.q[0].std;
-    console.log("src_ratings.std: " + s);
-    //translation
-    var t = ourData.ratings.worker.q[1].avg;
-    console.log("tsnl_ratings.avg: " + t);
-    var t = ourData.ratings.worker.q[1].std;
-    console.log("tsnl_ratings.std: " + t);
+    console.log("all_ratings.std: " + all_std);
+    // console.log(all_std);
+//source
+    var s_avg = ourData.ratings.worker.q[0].avg;
+    console.log("src_ratings.avg: " + s_avg);
+    var s_std = ourData.ratings.worker.q[0].std;
+    console.log("src_ratings.std: " + s_std);
+//translation
+    var t_avg = ourData.ratings.worker.q[1].avg;
+    console.log("tsnl_ratings.avg: " + t_avg);
+    var t_std = ourData.ratings.worker.q[1].std;
+    console.log("tsnl_ratings.std: " + t_std);
+//object
+    var ourResult = {"rated": rated, "unrated": unrated, "all_ratings.avg": all_avg, "all_ratings.std": all_std, "src_ratings.avg": s_avg, "src_ratings.std": s_std, "tsnl_ratings.avg": t_avg, "tsnl_ratings.std": t_std};
+    console.log(ourResult);
+//json
+    var jsonResult = JSON.stringify(ourResult);
+    console.log(jsonResult);
 };
 ourRequest.send();
+
